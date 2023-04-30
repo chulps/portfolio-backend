0f198f5aec6f12a42c072c5fe9d074db1820a33b
Chuck Howard <choward@foundrydigital.com>
2023-05-01 08:03:35 +0900

    define dotenv

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 dotenv.config();
 const app = express();
 app.use(cors({ origin: 'https://chulps.github.io/react-gh-pages/'})); // For production
