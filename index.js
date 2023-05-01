5a2f54790b596504e2d8f0c268de455f118a9370
Chuck Howard <choward@foundrydigital.com>
2023-05-01 09:53:43 +0900

    one more time

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-// app.use(cors({ origin: ['https://chulps.github.io/', 'https://chulps.github.io/react-gh-pages/'] })); // For production
 
 app.use(express.json());
 
